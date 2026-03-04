f4556b0343a51c0b2c917e443ae9a5c9de901d53
Simon Wülker <simon.wuelker@arcor.de>
2026-03-04 15:18:48 +0100

    script: Use stylo dom apis for querySelector/querySelectorAll (#42991)
    Stylo has ready-to-use apis for `Node::querySelector` and
    `Node::querySelectorAll`. We currently instead set up our own selector
    queries, which is neither as correct nor as performant, so this change
    makes servo use the stylo interfaces instead.
    
    This change contains a fair amount of unsafe code - review with caution!
    
    Notably, this makes the tab component from the astro framework work
    correctly in servo (https://starlight.astro.build/components/tabs/).
    I've seen that (broken) component on multiple websites.
    
    Fixes https://github.com/servo/servo/issues/41105
    Testing: New tests start to pass
    
    ---------
    
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- components/layout/layout_impl.rs
+++ components/layout/layout_impl.rs
@@ -26,6 +26,7 @@ use layout_api::{
     OffsetParentResponse, PhysicalSides, PropertyRegistration, QueryMsg, ReflowGoal,
     ReflowPhasesRun, ReflowRequest, ReflowRequestRestyle, ReflowResult,
     RegisterPropertyError, ScrollContainerQueryFlags, ScrollContainerResponse, TrustedNodeAddress,
+    with_layout_state,
 };
 use log::{debug, error};
 use malloc_size_of::{MallocConditionalSizeOf, MallocSizeOf, MallocSizeOfOps};
