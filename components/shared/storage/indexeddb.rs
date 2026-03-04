3b6b0ee12388b291f75b838b9ff7b020efb31a23
Taym Haddadi <haddadi.taym@gmail.com>
2026-03-04 14:33:45 +0100

    indexeddb: restore object store metadata on upgrade abort and sync connection  (#42786)
    The frontend connection metadata was not consistently synchronized with
    backend object store state during open/upgrade, and upgrade abort did
    not fully restore the connection object store set in all cases.
    
    Testing: More indexeddb tests should pass.
    part of https://github.com/servo/servo/issues/40983
    
    ---------
    
    Signed-off-by: Taym Haddadi <haddadi.taym@gmail.com>

--- components/shared/storage/indexeddb.rs
+++ components/shared/storage/indexeddb.rs
@@ -394,6 +394,9 @@ pub enum ConnectionMsg {
         id: Uuid,
         version: u64,
         upgraded: bool,
+        // https://w3c.github.io/IndexedDB/#upgrade-transaction-steps
+        // Step 3. Set transaction’s scope to connection’s object store set.
+        object_store_names: Vec<String>,
     },
     /// An upgrade transaction for a version started.
     Upgrade {
@@ -402,6 +405,9 @@ pub enum ConnectionMsg {
         version: u64,
         old_version: u64,
         transaction: u64,
+        // https://w3c.github.io/IndexedDB/#upgrade-transaction-steps
+        // Step 3. Set transaction’s scope to connection’s object store set.
+        object_store_names: Vec<String>,
     },
     /// A `versionchange` event should be fired for a connection.
     VersionChange {
