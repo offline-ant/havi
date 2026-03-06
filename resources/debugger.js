f325d9fa6bec48c0b7d161796dda3cf6936fe27b
eri <eri@igalia.com>
2026-03-06 17:05:56 +0100

    devtools: Fix step out and clearing stepping hooks (#43042)
    Make step out behave properly.
    
    Testing: DevTools tests and manual testing.
    Part of: #36027
    
    Signed-off-by: eri <eri@igalia.com>
    (cherry picked from commit 7d3e60d999f9fb61e44a92efe99d9ff9b5d94e79)

--- resources/debugger.js
+++ resources/debugger.js
@@ -206,7 +206,7 @@ function createFrameActor(frame, pipelineId) {
 
 function handlePauseAndRespond(frame, pauseReason) {
     dbg.onEnterFrame = undefined;
-    clearSteppingHooks();
+    clearSteppingHooks(frame);
 
     // Get the pipeline ID for this debuggee
     const pipelineId = debuggeesToPipelineIds.get(frame.script.global);
@@ -312,7 +312,14 @@ function makeSteppingHooks(steppingType, startFrame) {
                 return handlePauseAndRespond(startFrame, { type_: PAUSE_REASONS.RESUME_LIMIT });
             }
         },
-        onPop: () => {},
+        onPop: (completion) => {
+            this.reportedPop = true;
+            suspendedFrame = startFrame;
+            if (steppingType !== "finish") {
+                return handlePauseAndRespond(startFrame, completion);
+            }
+            attachSteppingHooks("next", startFrame);
+        },
     }
 }
 
@@ -328,7 +335,7 @@ function getNextStepFrame(frame) {
 // <https://searchfox.org/firefox-main/source/devtools/server/actors/thread.js#1235>
 function attachSteppingHooks(steppingType, frame) {
     if (steppingType === "finish" && frame.reportedPop) {
-      steppingType = "next";
+        steppingType = "next";
     }
 
     const stepFrame = getNextStepFrame(frame);
@@ -336,7 +343,7 @@ function attachSteppingHooks(steppingType, frame) {
         steppingType = "step";
     }
 
-    const { onEnterFrame, onPop, onStep } = makeSteppingHooks(
+    const { onEnterFrame, onStep, onPop } = makeSteppingHooks(
         steppingType,
         frame,
     );
@@ -352,6 +359,9 @@ function attachSteppingHooks(steppingType, frame) {
                 if (stepFrame.script) {
                     stepFrame.onStep = onStep;
                 }
+            case "finish":
+                stepFrame.onPop = onPop;
+                break;
         }
     }
 }
