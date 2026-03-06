944038d971573ce0fb27a5c5ecb6653b16866bbd
Brent Schroeter <github@brentsch.com>
2026-03-06 01:13:39 -0800

    tools: Support tab navigation via navigateTo, goBack, and goForward (#43026)
    Add support for navigation requests ("navigateTo", "goBack", and
    "goForward") over the Remote Debugging Protocol. These may be sent by a
    UI client in response to user input (for example the address bar in the
    Firefox inspector), or they can be used to automate navigation during
    unit tests.
    
    This currently only supports navigation within the URL domain at which
    servoshell is initially launched, due to a bug in servo's
    `BrowsingContextActor` implementation. (Unit tests covering a fix for
    that issue will depend on this change.)
    
    Testing: The behavior of all 3 new message types is covered by a new
    test case—`test_navigation`—in the devtools unit test suite.
    Fixes: #38668
    
    ---------
    
    Signed-off-by: Brent Schroeter <contact@brentsch.com>
    Co-authored-by: eri <eri@igalia.com>
    (cherry picked from commit 4c69d856f6c89de7688184eede8c56de09517949)

--- components/devtools/actors/root.rs
+++ components/devtools/actors/root.rs
@@ -118,6 +118,7 @@ struct ListProcessesResponse {
 pub(crate) struct DescriptorTraits {
     pub(crate) watcher: bool,
     pub(crate) supports_reload_descriptor: bool,
+    pub(crate) supports_navigation: bool,
 }
 
 #[derive(Serialize)]
